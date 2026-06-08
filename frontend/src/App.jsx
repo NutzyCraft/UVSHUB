c0fb445d71ae68ecf1aac5ff572a35e155e18f21
Soesh Cooray <soeshcooray@gmail.com>
2026-06-08 18:14:59 +0530

    Updated with new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing      from './pages/Landing';
-import Login        from './pages/Login';
-import Register     from './pages/Register';
-import Dashboard    from './pages/Dashboard';
-import Courses      from './pages/Courses';
-import CourseDetail from './pages/CourseDetail';
+import Landing      from './pages/home/Landing';
+import Courses      from './pages/home/Courses';
+import Instructors  from './pages/home/Instructors';
+import CourseDetail from './pages/home/CourseDetail';
+import StudentLogin from './pages/student/StudentLogin';
+import StudentRegister from './pages/student/StudentRegister';
+import StudentHome from './pages/student/StudentHome';
+import AdminDashboard from './pages/admin/AdminDashboard';
 import CustomCursor from './components/ui/CustomCursor';
 
 function App() {
@@ -13,11 +15,13 @@ function App() {
       <CustomCursor />
       <Routes>
         <Route path="/"            element={<Landing />}      />
-        <Route path="/login"       element={<Login />}        />
-        <Route path="/register"    element={<Register />}     />
-        <Route path="/dashboard"   element={<Dashboard />}    />
         <Route path="/courses"     element={<Courses />}      />
+        <Route path="/instructors" element={<Instructors />}  />
         <Route path="/courses/:id" element={<CourseDetail />} />
+        <Route path="/student/login"    element={<StudentLogin />} />
+        <Route path="/student/register" element={<StudentRegister />} />
+        <Route path="/student/home"     element={<StudentHome />} />
+        <Route path="/admin/dashboard"  element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
